efaa801312b47005bf769911c1f1ec1e3372cb75
Alan Borges <sr.alan.bispo@gmail.com>
2020-07-25 03:16:30 -0300

    Redux dashboard actions Now it fetchs API real data

--- react-frontend/src/dashboard/Dashboard.jsx
+++ react-frontend/src/dashboard/Dashboard.jsx
@@ -3,16 +3,24 @@ import ContentHeader from '../common/templates/ContentHeader';
 import Content from '../common/templates/Content';
 import ValueBox from '../common/widget/ValueBox';
 import Row from '../common/layout/Row';
+import { connect } from 'react-redux';
+import { getSummary } from './dashboardActions';
+import { bindActionCreators } from 'redux';
 
 class Dashboard extends React.Component {
+    componentDidMount() {
+        this.props.getSummary();
+    }
+
     render() {
+        const { credit, debt } = this.props
         return (
             <div>
                 <ContentHeader title='Dashboard' subtitle='Version 1.0' />
                 <Content>
                     <Row>
-                        <ValueBox cols='12 4' color='green' icon='bank' value='R$ 10,00' text='Total de créditos' />
-                        <ValueBox cols='12 4' color='red' icon='credit-card' value='R$ 10,00' text='Total de débitos' />
+                        <ValueBox cols='12 4' color='green' icon='bank' value={`R$ ${credit}`} text='Total de créditos' />
+                        <ValueBox cols='12 4' color='red' icon='credit-card' value={`R$ ${debt}`} text='Total de débitos' />
                         <ValueBox cols='12 4' color='blue' icon='money' value='R$ 10,00' text='Valor Consolidado' />
                     </Row>
                 </Content>
@@ -21,4 +29,6 @@ class Dashboard extends React.Component {
     }
 }
 
-export default Dashboard;
+const mapStateToProps = (state) => ({ credit: state.dashboard.summary.credit, debt: state.dashboard.summary.debt });
+const mapDispatchToProps = (dispatch) => bindActionCreators({ getSummary }, dispatch);
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
