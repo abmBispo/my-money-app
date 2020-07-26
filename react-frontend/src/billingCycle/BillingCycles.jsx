e93596d7833e2daa0a00b2f65c4cfe4447e4e033
Alan Borges <sr.alan.bispo@gmail.com>
2020-07-26 01:30:54 -0300

    Tabs

--- react-frontend/src/billingCycle/BillingCycles.jsx
+++ react-frontend/src/billingCycle/BillingCycles.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import ContentHeader from '../common/templates/ContentHeader';
+import Content from '../common/templates/Content';
+import Tabs from '../common/tab/Tabs';
+import TabsHeader from '../common/tab/TabsHeader';
+import TabsContent from '../common/tab/TabsContent';
+import TabHeader from '../common/tab/TabHeader';
 
-export default (props) => (
-    <div>
-        <h1>Billing Cycles</h1>
-    </div>
-);
+class BillingCycles extends React.Component {
+    render() {
+        return (
+            <div>
+                <ContentHeader title='Billing Cycles' subtitle='register' />
+                <Content>
+                    <Tabs>
+                        <TabsHeader>
+                            <TabHeader label='list' icon='bars' target='tabList' />
+                            <TabHeader label='Add' icon='plus' target='tabAdd' />
+                            <TabHeader label='Edit' icon='pencil' target='tabUpdate' />
+                            <TabHeader label='Delete' icon='trash-o' target='tabDelete' />
+                        </TabsHeader>
+                        <TabsContent>
+
+                        </TabsContent>
+                    </Tabs>
+                    Billing Cycles.
+                </Content>
+            </div>
+        )
+    }
+}
+
+export default BillingCycles;
---
+++ react-frontend/src/common/tab/TabHeader.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class TabHeader extends React.Component {
+    render() {
+        return (
+            <li>
+                <a href='javascript:;' tab-toggle='tab' data-target={this.props.target}>
+                    <i className={`fa fa-${this.props.icon}`}></i> {this.props.label}
+                 </a>
+            </li>
+        );
+    }
+}
+
+export default TabHeader;
